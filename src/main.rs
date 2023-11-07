35be6e99b8e8e55d6b98c81d18c590e3759c9566
Devin Pastoor <devin.pastoor@gmail.com>
2023-11-07 10:34:35 -0500

    wip: check in working version for andriy live

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use clap::{error, Parser};
-use pam_client::{Context, ConversationHandler, Flag, Session};
 use core::fmt;
-use std::io::{Stderr, BufReader};
+use pam_client::{Context, ConversationHandler, Flag, Session};
 use std::io::BufRead;
+use std::io::{BufReader, Stderr};
 use std::os::unix::process::CommandExt;
 use std::process::{Command, Stdio};
-use std::thread::{sleep, self};
+use std::thread::{self, sleep};
 use std::time::{Duration, Instant};
 use std::{env, fs};
 use users::get_user_by_name;
+use users::os::unix::UserExt;
 
 /// Define the command-line arguments and options using the clap derive pattern
 #[derive(Parser, Debug, Clone)]
@@ -43,11 +44,11 @@ fn setup_pam_context(
 
 use std::collections::HashMap;
 
-fn get_env_as_map() -> Result<HashMap<String, String>, String> {
+fn get_env_as_map(user: String) -> Result<HashMap<String, String>, String> {
     // Execute the command and capture the output
     let output = Command::new("su")
         .arg("-")
-        .arg("devin")
+        .arg(user)
         .arg("-c")
         .arg("printenv")
         .output()
@@ -72,57 +73,92 @@ fn get_env_as_map() -> Result<HashMap<String, String>, String> {
     Ok(env_map)
 }
 
-
-
 fn main() {
     // Parse the command-line arguments using the Args struct
     let args: Args = Args::parse();
 
-    let uid = get_user_by_name(&args.username.clone()).unwrap().uid();
-    println!("UID: {}", uid);
-    let mut ctx = setup_pam_context(args.username).unwrap_or_else(|e| {
-        println!("Error: {}", e);
-        std::process::exit(1);
-    });
-    let session = ctx.open_session(Flag::SILENT).unwrap_or_else(|e| {
-        println!("Error: {}", e);
-        std::process::exit(1);
-    });
-    println!("-------------- env vars process --------------");
-    for (key, value) in env::vars() {
-        println!("{key}: {value}");
-    }
-    println!("-------------- pam session vars session --------------");
-    // note this current returns nothing as we set no pam related env vars
-    for item in &session.envlist() {
-        println!("VAR: {}", item);
-    }
-
     let start_time = Instant::now();
-    let envs = get_env_as_map().unwrap_or_else(|e| {
+    let user = get_user_by_name(&args.username.clone()).unwrap_or_else(|| {
+        println!("Error: user {} does not exist", args.username);
+        std::process::exit(1);
+    });
+    println!("got user in {:?}", start_time.elapsed());
+    let home_dir = user.home_dir().to_str().unwrap_or_else(|| {
+        println!(
+            "Error: user {} does not have a home directory",
+            args.username
+        );
+        std::process::exit(1);
+    });
+    println!("UID: {}", user.uid());
+    println!("GUID: {}", user.primary_group_id());
+    println!("home_dir: {}", home_dir);
+
+
+    if let Ok(metadata) = fs::metadata(home_dir) {
+        if metadata.is_dir() {
+            println!("Home directory exists: {}", home_dir);
+        } else {
+            eprintln!("Error: {} is not a directory", home_dir);
+        }
+    } else {
+        println!("Error: Failed to access home directory at {}", home_dir);
+        let start_time = Instant::now(); 
+        let mut ctx = setup_pam_context(args.username.clone()).unwrap_or_else(|e| {
+            println!("Error: {}", e);
+            std::process::exit(1);
+        });
+        let _session = ctx.open_session(Flag::SILENT).unwrap_or_else(|e| {
+            println!("Error: {}", e);
+            std::process::exit(1);
+        });
+        println!("pam session created in {:?}", start_time.elapsed());
+        // TODO: lets not worry about pam env for now since we don't set any anyway
+        // need this to be in the proper ordering for this to actually work
+        // session.envlist().iter_tuples().for_each(|(key, value)| {
+        //     if !envs.contains_key(key.to_str().unwrap()) {
+        //         envs.insert(
+        //             key.to_str().unwrap().to_string(),
+        //             value.to_str().unwrap().to_string(),
+        //         );
+        //     }
+        // });
+        if let Ok(metadata) = fs::metadata(home_dir) {
+            if metadata.is_dir() {
+                println!("Home directory now exists: {}", home_dir);
+            } else {
+                eprintln!("Error: {} still doesn't exist", home_dir);
+            } 
+        }
+    }
+    let start_time = Instant::now();
+    let envs = get_env_as_map(args.username.clone()).unwrap_or_else(|e| {
         println!("Error: {}", e);
         std::process::exit(1);
     });
+
+    // add environmetn variables with those from the pam session
+    // TODO: should the pam env replace? is this the right order or should it be the inverse?
     let duration = start_time.elapsed();
     println!("Time elapsed for getting envs is: {:?}", duration);
 
     let mut child = Command::new("R")
+        //.arg(r#"-e 'names(Sys.getenv()); message("USER: ", Sys.getenv("USER"), "\nHOME: ", Sys.getenv("HOME"),  "\nPATH: ", Sys.getenv("PATH"))'"#)
         .arg("-e")
-        .arg("Sys.info()")
-        //.arg(r#"R -e 'names(Sys.getenv()); message("USER: ", Sys.getenv("USER"), "\nHOME: ", Sys.getenv("HOME"),  "\nPATH: ", Sys.getenv("PATH"))'"#)
+        .arg(r#"names(Sys.getenv()); message("USER: ", Sys.getenv("USER"), "\nHOME: ", Sys.getenv("HOME"),  "\nPATH: ", Sys.getenv("PATH"))"#)
         //.arg(r#"R -e 'names(Sys.getenv()); message("USER: ", Sys.getenv("USER"), "\nHOME: ", Sys.getenv("HOME"),  "\nPATH: ", Sys.getenv("PATH"))'"#)
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .env_clear()
         .envs(envs)
-        .uid(uid)
+        .uid(user.uid())
         .spawn()
         .expect("R command failed to start");
-
+    // println!("R process started with pid {}", child.id());
     let stdout_reader = child.stdout.take().expect("Failed to open stdout");
     let stderr_reader = child.stderr.take().expect("Failed to open stderr");
 
-    let stdout_thread = thread::spawn(move || {
+    let _stdout_thread = thread::spawn(move || {
         let stdout = BufReader::new(stdout_reader);
         for line in stdout.lines() {
             let line = line.expect("Failed to read stdout line");
@@ -130,7 +166,7 @@ fn main() {
         }
     });
 
-    let stderr_thread = thread::spawn(move || {
+    let _stderr_thread = thread::spawn(move || {
         let stderr = BufReader::new(stderr_reader);
         for line in stderr.lines() {
             let line = line.expect("Failed to read stderr line");
@@ -139,11 +175,10 @@ fn main() {
     });
 
     let status = child.wait().expect("Failed to wait for child process");
-    let duration = start_time.elapsed();
-    println!("Time elapsed for everything is: {:?}", duration);
-    stdout_thread.join().expect("stdout thread panicked");
-    stderr_thread.join().expect("stderr thread panicked");
+    // let duration = start_time.elapsed();
+    // println!("Time elapsed for everything is: {:?}", duration);
+    // stdout_thread.join().expect("stdout thread panicked");
+    // stderr_thread.join().expect("stderr thread panicked");
 
-    println!("Finished with status {:?}", status);
-
+    // println!("Finished with status {:?}", status);
 }
